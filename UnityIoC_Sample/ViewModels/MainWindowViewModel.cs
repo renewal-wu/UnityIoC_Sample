83c1de60766ef666239c9dfe96cab33f04eca9a1
Louis Wu <louis.wu@garmin.com>
2019-03-25 13:10:05 +0800

    Add DemoData.

--- UnityIoC_Sample/ViewModels/MainWindowViewModel.cs
+++ UnityIoC_Sample/ViewModels/MainWindowViewModel.cs
@@ -5,15 +5,16 @@ namespace UnityIoC_Sample.ViewModels
     public class MainWindowViewModel : BindableBase
     {
         private string _title = "Prism Application";
+
         public string Title
         {
             get { return _title; }
             set { SetProperty(ref _title, value); }
         }
 
-        public MainWindowViewModel()
+        public MainWindowViewModel(DemoData demoData)
         {
-
+            // DemoData will be generated automatically.
         }
     }
 }
